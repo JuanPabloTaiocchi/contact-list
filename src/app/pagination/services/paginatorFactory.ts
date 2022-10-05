e26e26de01a3794e767ec3c0a25c722a442c1cd3
juan pablo taiocchi <>
2022-10-05 11:59:48 +0200

    [refactor] Paginator Component Refactoring

--- src/app/pagination/services/paginatorFactory.ts
+++ src/app/pagination/services/paginatorFactory.ts
@@ -5,6 +5,16 @@ import { Modality } from '../interfaces/modality.interface';
 import { Paginator } from '../interfaces/paginator.interface';
 
 
+/**
+ * This is the class factory that creates the paginator based on the modality parameter.
+ * if I want use another modality, I have to create a new class that implements the Paginator interface
+ * @example
+ * getInstance(paginatorType: Modality): Paginator{
+ *   if (paginatorType === Modality.sqlite){ return new SqlPaginator(); }
+ *   if (paginatorType === Modality.firebase){ return new FirebasePaginator(); }
+ * }
+ * {@link https://refactoring.guru/design-patterns/factory-method}
+ */
 @Injectable()
 export class PaginatorFactory{
 
